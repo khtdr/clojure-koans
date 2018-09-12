22db3db5fd8fc5e9e046d045ce97bf506a915b56
Kay Oh <khtdr.com@gmail.com>
2018-09-11 18:33:31 -0600

    You have achieved clojure enlightenment. Namaste.

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,28 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
-  (loop [n   n
-         acc true]
+  (loop [n n   acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll acc []]
+    (if (empty? coll)
+      acc
+      (recur (drop-last 1 coll) (conj acc (last coll)))
+      )))
 
 (defn factorial [n]
-  __)
+  (loop [n n acc 1]
+    (if (= 1 n)
+      acc
+      (recur (- n 1) (* acc n))
+      )))
 
 (meditations
   "Recursion ends with a base case"
